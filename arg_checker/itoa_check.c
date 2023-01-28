859617d9c736cdc036c831500d9b9b1464d4b378
Sakata Atushi <satushi@c1r27s3.42tokyo.jp>
2023-01-28 09:05:56 +0900

    norm handling

--- arg_checker/itoa_check.c
+++ arg_checker/itoa_check.c
@@ -6,20 +6,11 @@
 /*   By: satushi <satushi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 07:58:00 by satushi           #+#    #+#             */
-/*   Updated: 2023/01/28 08:42:55 by satushi          ###   ########.fr       */
+/*   Updated: 2023/01/28 09:05:13 by satushi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
-
-static  int ft_isdigit(int num)
-{
-	if ('0' > num || '9' < num)
-	{
-		return (0);
-	}
-	return (1);
-}
 
 bool itoa_checker(char *string)
 {
